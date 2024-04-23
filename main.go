e8585133a2b9b424d46b95e889f267233caa2fe5
EC2 Default User <ec2-user@ip-172-30-4-234.us-east-2.compute.internal>
2024-04-23 22:08:23 +0000

    iterating through output hierarchy to insert into database (function not yet complete)

--- main.go
+++ main.go
@@ -23,6 +23,47 @@ import (
 )
 
 func main() {
+	//MODIFYING TO USE API DIRECTLY FROM THE CODE AS OPPOSED TO HTTP
+
+	//Goal 1: single out only the hierarchy (process) from the output
+	//note output only contains one hierarchy/process
+	name := "test"
+	output, err := ModelData(name)
+	if err != nil {
+		fmt.Println("error")
+	}
+	// fmt.Println(output.Hierarchy)
+
+	//Goal 2: Store the name of the process, the stages, and any measures
+	processName := output.Hierarchy.Process
+	fmt.Println("Here is process name:" + processName)
+
+	process_stages := output.Hierarchy.Stages
+
+	//this outer loop traverses stages
+	for _, stage := range process_stages {
+		//fmt.Println("Element at index", i, ":", value)
+		//this outer loop traverses stages
+		operations := stage.Operations
+
+		//this loop traverses operations
+		for _, operation := range operations {
+			//fmt.Println("Element at index", i, ":", value)
+			actions := operation.Actions
+			//this loop traverses operations
+			for _, action := range actions {
+				//fmt.Println("Element at index", i, ":", value)
+				//insertAction(action)
+				measures := action.Measures
+
+				for _, measure := range measures {
+					//insertMeasure(measure)
+				}
+			}
+
+		}
+	}
+
 	// Creating the connection to the server.
 	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("wss://neptunedbinstance-6sb274y70mqt.c7a08meoiuv1.us-east-2.neptune.amazonaws.com:8182/gremlin",
 		func(settings *gremlingo.DriverRemoteConnectionSettings) {
