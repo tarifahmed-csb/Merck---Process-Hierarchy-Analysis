a96c491eb6d4ab3401ff5c2682169b34720feb6a
EC2 Default User <ec2-user@ip-172-30-4-234.us-east-2.compute.internal>
2024-04-23 22:01:15 +0000

    connection & queries to neptune successful

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module digitalmmd-ppx-goapi-template
 
-go 1.20
+go 1.21
+
+toolchain go1.22.1
 
 require (
 	github.com/gocarina/gocsv v0.0.0-20230616125104-99d496ca653d
@@ -11,17 +13,21 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/apache/tinkerpop/gremlin-go/v3 v3.7.2 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
 	github.com/go-openapi/spec v0.20.14 // indirect
 	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
---
+++ lastMain.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"digitalmmd-ppx-goapi-template/docs"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	_ "embed"
+
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+//========================= Swagger API Header Fields =====================
+// @title Golang API template
+// @description golang API Template
+
+// @contact.name Author Email
+// @contact.email author@lehigh.edu
+
+// @tag.name metadata
+// @tag.description endpoints with metadata about the API
+
+// @tag.name dataModels
+// @tag.description data model endpoints
+
+// @host localhost:8080
+
+// ========================= End Swagger Definition =======================
+
+//go:embed version.txt
+var ver string
+var version = fmt.Sprintf("ver 0.0.%v", ver)
+
+func lastMain() {
+
+	docs.SwaggerInfo.Version = version
+	e := echo.New()
+
+	// optional - show version of API in log
+	fmt.Printf("API Template - %v", version)
+
+	// include if you are going to include swagger documenation
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// basepage endpoint
+	e.GET("/", homeLink)
+
+	// metadata endpoints
+	e.GET("/version", versionEndpt)
+
+	// model generation endpoints
+	e.GET("/v1/dataModel/:name", dataModelEndpt)
+
+	// report API output with detailed logs
+	e.Use(middleware.Logger())
+	// enable CORS support (required in Merck network)
+	e.Use(middleware.CORS())
+	// control port ID through environmental variable - useful for remote deploy
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+func homeLink(c echo.Context) error {
+
+	// put whatever HTML you'd like to appear on the welcome screen
+	// this is totally optional
+
+	welcome := fmt.Sprintln("<div style='width:100%;text-align:center;font-size:30px;font-family:sans-serif;margin-top:50px;'>" +
+		"<p style='margin:0 200px;color:gray;border-bottom:1px solid gray;'>" +
+		"Merck DB Model API" +
+		"</p>" +
+		"<p style='font-size:60%;color:gray;margin:0;'>" + version + "</p>" +
+		"<p style='color:darkblue;font-size:180%;font-weight:400;margin-top:20px;'>" +
+		"go for it!" +
+		"</p>" +
+		"<a href='/swagger/index.html' style='font-size:70%;color:darkgray;text-decoration:none;'>api documentation</a>" +
+		"</div>")
+
+	return c.HTML(http.StatusOK, welcome)
+}
