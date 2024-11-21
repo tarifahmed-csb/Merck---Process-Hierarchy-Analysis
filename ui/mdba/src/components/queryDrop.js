1029772e2cf771e463a433f61158635bed9ba6d1
AidenAstle <69543550+AidenAstle@users.noreply.github.com>
2024-11-21 15:12:19 -0500

    addition of proc name entries and logDat.txt file

--- ui/mdba/src/components/queryDrop.js
+++ ui/mdba/src/components/queryDrop.js
@@ -37,49 +37,52 @@ async function query(type) {
     var procName = document.getElementById('process').value
     var e = document.getElementById(type);
     var query_type = e.value;
+    var urls  = ["http://localhost:1010/query", "http://localhost:1011/query", "http://localhost:1012/query"]
+    // Loops through the declared URLs ^ and makes a request and awaits a response from all of them
     console.log(query_type);
+    var xhr = new XMLHttpRequest();
     if (query_type == 'measures') { // --------------------------------- Measurement Query -----------------------------------------------
-        var xhr = new XMLHttpRequest();
-        var url = "http://localhost:1010/query";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = JSON.parse(xhr.responseText);
-                console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
-                alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
-            }
-        };
-        var data = JSON.stringify({"type": "measures", "name": "test"});
-        xhr.send(data);
+        for (var i = 0; i < urls.length; i++){
+            xhr.open("POST", urls[i], true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
+                    alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
+                }
+            };
+            var data = JSON.stringify({"type": "measures", "name": procName});
+            xhr.send(data);
+        }
     } else if (query_type == 'processes') { // --------------------------------- Process Query --------------------------------------
-        var xhr = new XMLHttpRequest();
-        var url = "http://localhost:1010/query";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = JSON.parse(xhr.responseText);
-                console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
-                alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
-            }
-        };
-        var data = JSON.stringify({"type": "process", "name": "***"});
-        xhr.send(data);
+        for (var i = 0; i < urls.length; i++){
+            xhr.open("POST", urls[i], true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
+                    alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
+                }
+            };
+            var data = JSON.stringify({"type": "process", "name": procName});
+            xhr.send(data);
+    }
     } else if (query_type == 'rawMat') { // --------------------------------- Raw Material Query ---------------------------------------
-        var xhr = new XMLHttpRequest();
-        var url = "http://localhost:1010/query";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = JSON.parse(xhr.responseText);
-                console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
-                alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
-            }
-        };
-        var data = JSON.stringify({"type": "rawMat", "name": "test"});
-        xhr.send(data);
+        for (var i = 0; i < urls.length; i++){
+            xhr.open("POST", urls[i], true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    console.log("Status: "+json.status + ", Time: " + json.time + ", Results: \n"+ json.results +"\nError: " +json.error);
+                    alert("Status: "+json.status + "\nTime: " + json.time + ", Results: \n"+ json.results + "\nError: " +json.error);
+                }
+            };
+            var data = JSON.stringify({"type": "rawMat", "name": procName});
+            xhr.send(data);
+        }
     } else {
         console.log('INVALID TYPE')
     }
